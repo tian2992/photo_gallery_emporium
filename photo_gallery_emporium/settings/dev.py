32d3331544dd3f0c161fe5655f6f91015e458081
tian <code@sebastianoliva.com>
2022-09-19 03:44:19 -0600

    adding wagtail

---
+++ photo_gallery_emporium/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-y=rqrmlh41ga%d(f20#6ocmv0vd&so-7acia=r3w2py+)20ime"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
