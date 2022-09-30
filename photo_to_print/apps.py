e98eb126fb8d73aab2d54e9e45538c28bb9b454a
Sebastian Oliva <code@sebastianoliva.com>
2022-09-30 01:53:49 -0600

    photo to print add

---
+++ photo_to_print/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PhotoToPrintConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'photo_to_print'
---
+++ photo_to_print/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+
+class Reservation(models.Model):
+    pass
