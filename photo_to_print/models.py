fb56333f571927c79e4dfcca77532d30270c073a
Sebastian Oliva <code@sebastianoliva.com>
2022-10-02 20:52:58 -0600

    photo app models

--- photo_to_print/models.py
+++ photo_to_print/models.py
@@ -2,5 +2,25 @@ from django.db import models
 
 # Create your models here.
 
+
 class Reservation(models.Model):
     pass
+
+
+class PaymentStatus(models.TextChoices):
+    NOT_PAID = 'not_paid', 'Not Paid'  # TODO: make this  _('translatable')
+    PROCESSING = 'processed', 'Processing'
+    PAID = 'paid', 'Paid'
+    COUPON = 'coupon', 'Coupon'
+
+
+class Payment(models.Model):
+    status = models.CharField(
+        max_length=16,
+        choices=PaymentStatus.choices,
+        default=PaymentStatus.NOT_PAID,
+    )
+
+    def get_payment_status(self) -> PaymentStatus:
+        return PaymentStatus[self.status]
+
