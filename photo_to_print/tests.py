25e911edf9506b26045e7e8d24a86db28bc7a2d6
tian <code@sebastianoliva.com>
2022-10-07 01:39:36 -0600

    base test

--- photo_to_print/tests.py
+++ photo_to_print/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+
+
+class TestRedeemPic(TestCase):
+
+    def run(self):
+        # TODO:
+        # setup new pics
+        # setup an order
+        # store the reservation
+        pass
